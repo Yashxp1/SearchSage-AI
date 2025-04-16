0d9461794bf93502d1679c42ee43ab64952eae3f
Yash Raj <kyashraj991@gmail.com>
2025-04-16 10:44:27 +0530

    create signup function

--- api/src/controllers/user.conroller.ts
+++ api/src/controllers/user.conroller.ts
@@ -1,12 +1,60 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-import {z} from 'zod'
+import { z } from 'zod';
+import prisma from '../db/prisma';
+import { error } from 'console';
+import generateToken from '../utils/generateToken';
 
+const userSchema = z.object({
+  name: z.string().trim().min(1, 'Name is required'),
+  username: z.string().min(1, 'Username is required'),
+  password: z.string().min(1, 'Password is required'),
+});
 
 export const signup = async (req: Request, res: Response) => {
   try {
-    
-  } catch (error) {
-    
+    const result = userSchema.safeParse(req.body);
+
+    if (!result.success) {
+      const errors = result.error.flatten().fieldErrors;
+      return res.status(400).json({ errors });
+    }
+
+    const { name, username, password } = result.data;
+
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        error: 'Username already exists',
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    if (newUser) {
+      generateToken(newUser.id, res);
+
+      res.status(201).json({
+        id: newUser.id,
+        name: newUser.name,
+        username: newUser.username,
+      });
+    } else {
+      res.status(400).json({ success: false, error: 'Invalid user data' });
+    }
+  } catch (error: any) {
+    console.log('Error in signup controller', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-}
+};
